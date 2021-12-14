3ebd490bfab58c2f2be0373789bbe2434115bca1
oluwatayo <oluwatayoadegboye@gmail.com>
2021-12-14 07:04:57 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Oluwatayo Adegboye on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
